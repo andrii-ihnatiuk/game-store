6b8f145d26786890ed71832a84794d6e9f54a46c
andrii_ihnatiuk <andrii_ihnatiuk@epam.com>
2023-10-09 22:58:28 +0300

    nfr: added response caching

--- GameStore/GameStore.API/Program.cs
+++ GameStore/GameStore.API/Program.cs
@@ -1,13 +1,25 @@
 using GameStore.API.Middlewares;
 using GameStore.Services.Configuration;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opts =>
+{
+    opts.CacheProfiles.Add(
+        "OneMinuteCache",
+        new CacheProfile()
+        {
+            Duration = 60,
+            Location = ResponseCacheLocation.Any,
+            NoStore = builder.Environment.IsDevelopment(), // no store cache in development environment
+        });
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddResponseCaching();
 
 builder.Services.Configure<RouteOptions>(options =>
 {
@@ -25,6 +37,8 @@ if (app.Environment.IsDevelopment())
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.UseHttpsRedirection();
+
+app.UseResponseCaching();
 
 app.UseAuthorization();
 
